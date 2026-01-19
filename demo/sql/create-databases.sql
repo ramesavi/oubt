85cf66e0ea6e55c0c34efe84c83822cd13b35adb
Ramesh Subramani <ramesh_subramani@apple.com>
2026-01-19 12:02:42 -0800

    refactored sql files

---
+++ demo/sql/create-databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE gold LOCATION 's3://week-4-oubt/gold';
+CREATE DATABASE master LOCATION 's3://week-4-oubt/master';
+CREATE DATABASE silver LOCATION 's3://week-4-oubt/silver/';
+CREATE DATABASE debug LOCATION 's3://week-4-oubt/debug/';
